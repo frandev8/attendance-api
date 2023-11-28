5b2c507fdab24e7e3dfcc0ffda0fa2caf0bb1a07
Frandev <francisnyamekye48@gmail.com>
2023-11-28 10:54:33 +0000

    adjusted the allowed origin

--- conf/allowOrigins.js
+++ conf/allowOrigins.js
@@ -1,9 +1,9 @@
 const allowedOrigin = [
   "http://localhost:3000",
   "http://localhost:5173",
-  "https://www.kasapa-media.onrender.com/",
-  "https://kasapa-media.onrender.com/",
-  "https://kasapa-media.onrender",
+  "https://www.kasapa-media-bbhx.onrender.com/",
+  "https://kasapa-media-bbhx.onrender.com",
+  "https://kasapa-media-bbhx.onrender",
 ];
 
 const corsOptions = {
